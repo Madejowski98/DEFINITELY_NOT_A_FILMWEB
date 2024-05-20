2060e86b3adb0aaacad0fa441555a1782470d334
Madejowski98 <madejowski.s@gmail.com>
2024-05-20 20:26:17 +0200

    added a Review Model to models.py and added user_view app to settings.py

--- user_view/models.py
+++ user_view/models.py
@@ -1,3 +1,21 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import CharField, Model, IntegerField, DecimalField, DateTimeField, ForeignKey
+from django.utils import timezone
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 
-# Create your models here.
+"""
+    I have created a Review model imported from django.db.models that will take user_id as a IntegerField,
+    movie_id that will take a IntergerField, rating taking a IntegerField with minimum value of 1 and maximum of 10,
+    review taking a CharField with a maximum length of 255 characters,
+    created_at which will be a DateTimeField,
+    added_by a ForeignKey to User model.
+"""
+class Review(Model):
+    user_id = models.IntegerField()
+    movie_id = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    review = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
