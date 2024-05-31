f180b24d82ab42b6c5150a4e87aa3787d3449b9f
Bartek <abno9444@gmail.com>
2024-05-31 19:26:31 +0200

    Revert "added function to edit / delete review added by user"
    This reverts commit 4d16f89c0ad1eab31478b0246db31d3fb58c6f7a.

--- movies_app/views.py
+++ movies_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Movie, Review, Article, Genre
 from .forms import MovieForm, ReviewForm, ArticleForm, GenreForm
-from django.contrib import messages
 
 
 ## MOVIE VIEWS
