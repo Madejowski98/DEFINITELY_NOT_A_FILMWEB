5bd42801d9c351378ce0077cbd31c920ad3d31e0
Bartek <abno9444@gmail.com>
2024-06-04 15:35:16 +0200

    added photo to article model, updated templates for articles

--- movies_app/models.py
+++ movies_app/models.py
@@ -71,6 +71,7 @@ class Article(models.Model):
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
     approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to="media/", null=True, blank=True)
 
     def __str__(self):
         return self.title
